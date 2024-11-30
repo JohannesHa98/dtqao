646270d17afb9c5fd760d346ab221bf583b792f1
JohannesHa98 <johannesh1998@hotmail.com>
2024-11-30 21:17:13 +0100

    Fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,28 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useState } from "react";
-import axios from "axios";
+import Image from 'next/image';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [selectedServer, setSelectedServer] = useState("");
-  const [selectedRegion, setSelectedRegion] = useState("eu");
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedServer, setSelectedServer] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('eu');
   const [showMessage, setShowMessage] = useState(null);
   const [searchResult, setSearchResult] = useState(null);
   const [hasSearched, setHasSearched] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const [lastModifiedDate, setLastModifiedDate] = useState(null);
-  const [randomImage, setRandomImage] = useState(null);
 
-  const onlineImages = ["/minussocial.png"];
+  const onlineImages = [
+    '/minussocial.png',
+  ];
 
-  const ragequitImages = ["/rest.png", "/knuckles.png"];
+  const ragequitImages = [
+    '/rest.png',
+    '/knuckles.png',
+  ];
 
   const getRandomImage = (imageList) => {
     const randomIndex = Math.floor(Math.random() * imageList.length);
@@ -28,36 +32,32 @@ export default function Home() {
   const getAccessToken = async () => {
     try {
       const response = await axios.post(
-        "https://us.battle.net/oauth/token",
+        'https://us.battle.net/oauth/token',
         null,
         {
           params: {
-            grant_type: "client_credentials",
+            grant_type: 'client_credentials',
           },
           auth: {
             username: process.env.NEXT_PUBLIC_BLIZZARD_CLIENT_ID,
             password: process.env.NEXT_PUBLIC_BLIZZARD_CLIENT_SECRET,
           },
           headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
+            'Content-Type': 'application/x-www-form-urlencoded',
           },
         }
       );
       return response.data.access_token;
     } catch (error) {
-      console.error(
-        "Error fetching access token:",
-        error.response || error.message
-      );
-      throw new Error("Failed to fetch access token");
+      console.error('Error fetching access token:', error.response || error.message);
+      throw new Error('Failed to fetch access token');
     }
   };
 
   const handleSearch = async () => {
     if (searchQuery.trim() && selectedServer.trim()) {
       setLoading(true);
-      setErrorMessage("");
-      setRandomImage(getRandomImage(onlineImages));
+      setErrorMessage('');
 
       const apiUrl = `https://${selectedRegion}.api.blizzard.com/profile/wow/character/${selectedServer}/${searchQuery}/statistics?namespace=profile-${selectedRegion}&locale=en_US&timestamp=${Date.now()}`;
 
@@ -70,10 +70,10 @@ export default function Home() {
           },
         });
 
-        const lastModified = response.headers["last-modified"];
+        const lastModified = response.headers['last-modified'];
         const formattedDate = lastModified
           ? new Date(lastModified).toLocaleString()
-          : "Unknown";
+          : 'Unknown';
         setLastModifiedDate(formattedDate);
 
         if (response.data) {
@@ -85,21 +85,21 @@ export default function Home() {
             const timeDifference = (now - lastModifiedDate) / (1000 * 60);
 
             if (timeDifference > 10) {
-              setShowMessage("message1");
+              setShowMessage('message1');
             } else {
-              setShowMessage("message2");
+              setShowMessage('message2');
             }
           } else {
-            setShowMessage("message1");
+            setShowMessage('message1');
           }
         } else {
-          setErrorMessage("No data found for this character.");
+          setErrorMessage('No data found for this character.');
         }
       } catch (error) {
         if (error.response && error.response.status === 404) {
-          setErrorMessage("That guy does not seem to exist.");
+          setErrorMessage('That guy does not seem to exist.');
         } else {
-          setErrorMessage("Failed to fetch data. Please try again.");
+          setErrorMessage('Failed to fetch data. Please try again.');
         }
       } finally {
         setLoading(false);
@@ -109,17 +109,17 @@ export default function Home() {
   };
 
   const handleBack = () => {
-    setSearchQuery("");
-    setSelectedServer("");
-    setSelectedRegion("eu");
+    setSearchQuery('');
+    setSelectedServer('');
+    setSelectedRegion('eu');
     setShowMessage(null);
     setSearchResult(null);
     setHasSearched(false);
-    setErrorMessage("");
+    setErrorMessage('');
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       handleSearch();
     }
   };
@@ -173,29 +173,30 @@ export default function Home() {
             <div className="text-red-500 font-bold">{errorMessage}</div>
           )}
 
-          {showMessage === "message1" && !loading && !errorMessage && (
+          {showMessage === 'message1' && !loading && !errorMessage && (
             <div className="text-5xl font-bold text-white mt-4">
               <span className="text-green-400 uppercase">
                 {searchResult?.character?.name}
-              </span>{" "}
+              </span>{' '}
               QUEUED ANOTHER 😭
               <div>
                 <Image
-                  src={randomImage}
+                  src={getRandomImage(onlineImages)}
                   alt="Online Image"
                   width={256}
                   height={256}
                   className="rounded-lg absolute right-72 transform rotate-[330deg]"
+                  priority={true}
                 />
               </div>
             </div>
           )}
 
-          {showMessage === "message2" && !loading && !errorMessage && (
+          {showMessage === 'message2' && !loading && !errorMessage && (
             <div className="text-5xl font-bold text-white mt-4">
               <span className="text-red-400 uppercase">
                 {searchResult?.character?.name}
-              </span>{" "}
+              </span>{' '}
               RAGEQUIT 😆
               {lastModifiedDate && (
                 <div className="mt-2 text-lg text-white">
@@ -204,11 +205,12 @@ export default function Home() {
               )}
               <div>
                 <Image
-                  src={randomImage}
+                  src={getRandomImage(ragequitImages)}
                   alt="Ragequit Image"
                   width={256}
                   height={256}
                   className="rounded-lg absolute right-72 transform rotate-[330deg]"
+                  priority={true}
                 />
               </div>
             </div>
